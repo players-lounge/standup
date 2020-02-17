58ae01fb9d082eed9df7a8c729357598b2fe54e5
benjaminkay93 <ben.kay01@hotmail.co.uk>
2020-02-17 17:15:06 +0000

    flip doms name

--- components/Name/Name.jsx
+++ components/Name/Name.jsx
@@ -4,15 +4,29 @@ import Paragraph from 'components/Paragraph'
 
 const StyledDiv = styled.div`
   padding: 0.5rem;
-  border: 1px solid ${({ theme }) => theme.colors.border};;
+  border: 1px solid ${({ theme }) => theme.colors.border};
 `
 
-const Name = ({ name, className }) => (
-  <StyledDiv className={className}>
-    <Paragraph>
-      {name}
-    </Paragraph>
-  </StyledDiv>
-)
+const AustrailianDiv = styled.div`
+  padding: 0.5rem;
+  border: 1px solid ${({ theme }) => theme.colors.border};
+  transform: rotate(180deg);
+`
+
+const Name = ({ name, className }) => name === 'Dom' || name === '✅ Dom'
+  ? (
+    <AustrailianDiv className={className}>
+      <Paragraph>
+        {name}
+      </Paragraph>
+    </AustrailianDiv>
+  )
+  : (
+    <StyledDiv className={className}>
+      <Paragraph>
+        {name}
+      </Paragraph>
+    </StyledDiv>
+  )
 
 export default Name
